16c25047cef91eb04f0d40b65a777ee53aeda1f6
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-04-07 05:58:02 +0530

    Update Link, Set server data in testText

--- app/src/main/java/in/ghostreborn/test2/MainServerAsync.java
+++ app/src/main/java/in/ghostreborn/test2/MainServerAsync.java
@@ -2,15 +2,38 @@ package in.ghostreborn.test2;
 
 import android.os.AsyncTask;
 
-public class MainServerAsync extends AsyncTask<Void, Void,Void> {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainServerAsync extends AsyncTask<Void, Void, String> {
     @Override
-    protected Void doInBackground(Void... voids) {
-        return null;
+    protected String doInBackground(Void... voids) {
+        try{
+            URL url = new URL(Constants.HLS_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    protected void onPostExecute(Void unused) {
-        super.onPostExecute(unused);
-        MainActivity.testText.setText("DONE!");
+    protected void onPostExecute(String test) {
+        super.onPostExecute(test);
+        MainActivity.testText.setText(test);
     }
 }
